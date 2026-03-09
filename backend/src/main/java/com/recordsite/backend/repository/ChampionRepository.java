133a8d123db513ae9d7719445fde376058638c94
KimGyuhyun <kgh980605@gmail.com>
2026-03-09 20:26:28 +0900

    feat: Adding a data basic settings repository

---
+++ backend/src/main/java/com/recordsite/backend/repository/ChampionRepository.java
@@ -0,0 +1,9 @@
+package com.recordsite.backend.repository;
+
+import com.recordsite.backend.entity.Champion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChampionRepository extends JpaRepository<Champion, Long> {
+
+    boolean existsByChampionId(String championId); // DB에 해당 championId가 이미 있는지 체크 (중복 저장 방지용)
+}
