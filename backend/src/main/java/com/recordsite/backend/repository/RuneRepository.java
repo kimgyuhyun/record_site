f18ce4f164048eac60e24c545070143b0e4ce1d2
KimGyuhyun <kgh980605@gmail.com>
2026-03-12 17:38:05 +0900

    feat: Rune Repo add

--- backend/src/main/java/com/recordsite/backend/repository/RuneRepository.java
+++ backend/src/main/java/com/recordsite/backend/repository/RuneRepository.java
@@ -1,10 +1,8 @@
 package com.recordsite.backend.repository;
 
 import com.recordsite.backend.entity.Rune;
-import com.recordsite.backend.entity.RunePath;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface RuneRepository extends JpaRepository<Rune, Long> {
 
