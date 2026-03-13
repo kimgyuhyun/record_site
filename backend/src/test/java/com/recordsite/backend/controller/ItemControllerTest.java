267464aeeb681c97cd16d2901d4e4e2f28f2e5a3
KimGyuhyun <kgh980605@gmail.com>
2026-03-13 13:44:54 +0900

    feat: Add a test to view the entire list of items, rune paths, and runes

---
+++ backend/src/test/java/com/recordsite/backend/controller/ItemControllerTest.java
@@ -0,0 +1,48 @@
+package com.recordsite.backend.controller;
+
+import com.recordsite.backend.dto.ItemDto;
+import com.recordsite.backend.service.ItemService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ItemController.class)
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ItemService itemService;
+
+    @Test
+    void getItemList() throws Exception {
+        ItemDto item1 = new ItemDto();
+        ItemDto item2 = new ItemDto();
+        item1.setItemName("롱소드");
+        item2.setItemName("도란검");
+
+        when(itemService.findAllItemList()).thenReturn(List.of(item1, item2));
+
+        mockMvc.perform(get("/api/items"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].itemName").value("롱소드"))
+                .andExpect(jsonPath("$[1].itemName").value("도란검"))
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+}
---
+++ backend/src/test/java/com/recordsite/backend/controller/RuneControllerTest.java
@@ -0,0 +1,68 @@
+package com.recordsite.backend.controller;
+
+import com.recordsite.backend.dto.RuneDto;
+import com.recordsite.backend.dto.RunePathDto;
+import com.recordsite.backend.service.RuneService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(RuneController.class)
+class RuneControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private RuneService runeService;
+
+
+    @Test
+    void getRunePathListTest() throws Exception {
+        RunePathDto rp1 = new RunePathDto();
+        RunePathDto rp2 = new RunePathDto();
+        rp1.setRunePathNameKor("정밀");
+        rp2.setRunePathNameKor("지배");
+
+        when(runeService.findAllRunePathList()).thenReturn(List.of(rp1,rp2));
+
+        mockMvc.perform(get("/api/runePaths"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].runePathNameKor").value("정밀"))
+                .andExpect(jsonPath("$[1].runePathNameKor").value("지배"))
+                .andExpect(jsonPath("$.length()").value(2));
+
+    }
+
+    @Test
+    void getRuneListTest() throws Exception {
+        RuneDto r1 = new RuneDto();
+        RuneDto r2 = new RuneDto();
+        r1.setRuneNameKor("정복자");
+        r2.setRuneNameKor("감전");
+
+        when(runeService.findAllRuneList()).thenReturn(List.of(r1,r2));
+
+        mockMvc.perform(get("/api/runePaths/runes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].runeNameKor").value("정복자"))
+                .andExpect(jsonPath("$[1].runeNameKor").value("감전"))
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+}
