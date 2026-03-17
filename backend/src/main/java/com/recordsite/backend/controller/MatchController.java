20e76725ca5781090eac425c554f9f8de6aad84d
KimGyuhyun <kgh980605@gmail.com>
2026-03-17 21:50:41 +0900

    feat: Add a feature to combine match and participant data

--- backend/src/main/java/com/recordsite/backend/controller/MatchController.java
+++ backend/src/main/java/com/recordsite/backend/controller/MatchController.java
@@ -1,4 +1,24 @@
 package com.recordsite.backend.controller;
 
+import com.recordsite.backend.dto.MatchDto;
+import com.recordsite.backend.dto.MatchSummaryDto;
+import com.recordsite.backend.service.MatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class MatchController {
+    private final MatchService matchService;
+
+    @GetMapping("api/summoners/{puuid}/matches")
+    public List<MatchSummaryDto> getMatchSummaryList(@PathVariable String puuid) {
+        return matchService.getMatchSummaryListByPuuid(puuid);
+    }
 }
