72b7272aca93ce63cbd7b0cb3fcfa48814bc2c62
KimGyuhyun <kgh980605@gmail.com>
2026-03-21 21:42:54 +0900

    feat: Definition of a method to retrieve users who participated in a match, sorted in descending order by matchId

--- backend/src/main/java/com/recordsite/backend/repository/ParticipantRepository.java
+++ backend/src/main/java/com/recordsite/backend/repository/ParticipantRepository.java
@@ -2,12 +2,33 @@ package com.recordsite.backend.repository;
 
 import com.recordsite.backend.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-    List<Participant> findAllByPuuid(String puuid);
+
+    @Query("""
+            select p
+            from Participant p
+            join fetch p.match
+            where p.puuid = :puuid
+            order by p.match.gameCreation desc
+            """)
+    List<Participant> findAllParticipantListByPuuid(String puuid);
     // puuid 가 같은 모든 참가자 = 그 계정이 참가한 모든 경기
+
+    @Query("""
+            select p
+            from Participant p
+            where p.match.matchId = : matchId
+            order by p.participantId asc
+            """)
+    List<Participant> findByMatchIdForParticipantList(@Param("matchId") String matchId);
+    // matchId로 해당판에 참가자 목록을 asc로 정렬해서 가져옵니다.
+    // @Param은 컴파일 시 인자가 arg0, arg1 등으로 바뀔수있기떄문에 바인딩 정확하게 하기위해 명시하는것
+    // 여긴 안해도되긴함 그냥 해본것
 }
