08b639a51804b9ef0e743bc85a4270d10dcfcf29
KimGyuhyun <kgh980605@gmail.com>
2026-06-11 18:50:34 +0900

    feat: Add QueryDSL dependency / config

--- backend/src/main/java/com/recordsite/backend/config/AppConfig.java
+++ backend/src/main/java/com/recordsite/backend/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.recordsite.backend.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,4 +16,12 @@ public class AppConfig {
     }
     // RestTemplate는 Java에서 HTTP 요청을 보내는 도구
     // 브라우저에서 URL 치고 들어가는 걸 코드로 하는것
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
 }
