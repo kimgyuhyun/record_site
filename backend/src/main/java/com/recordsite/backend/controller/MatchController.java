7c7729759064999a0ed8afda50631e967046446a
KimGyuhyun <kgh980605@gmail.com>
2026-06-12 16:07:52 +0900

    feat: Add refresh button API

--- backend/src/main/java/com/recordsite/backend/controller/MatchController.java
+++ backend/src/main/java/com/recordsite/backend/controller/MatchController.java
@@ -1,9 +1,11 @@
 package com.recordsite.backend.controller;
 
-import com.recordsite.backend.dto.MatchListDto;
+import com.recordsite.backend.dto.MatchRecordDto;
 import com.recordsite.backend.dto.MatchSummaryDto;
 import com.recordsite.backend.service.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +18,19 @@ public class MatchController {
     private final MatchService matchService;
 
     @GetMapping(params = "puuid")
-    public ResponseEntity<List<MatchListDto>> getMatchList(@RequestParam String puuid) throws Exception {
-        return ResponseEntity.ok(matchService.getMatchListByPuuid(puuid));
+    public ResponseEntity<Page<MatchRecordDto>> getMatchList(
+            @RequestParam String puuid, Pageable pageable) throws Exception {
+        Page<MatchRecordDto> matchRecordDto = matchService.getMatchRecordsByPuuid(puuid, pageable);
+        return ResponseEntity.ok(matchRecordDto);
     }
 
     @GetMapping("/{matchId}/summary")
     public ResponseEntity<List<MatchSummaryDto>> getMatchSummaryList(@PathVariable String matchId) throws Exception {
-        return ResponseEntity.ok(matchService.getMatchSummaryListByMatchId(matchId));
+        return ResponseEntity.ok(matchService.getParticipantSummaryListByMatchId(matchId));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Integer> refreshMatches(@RequestParam String puuid) {
+        return ResponseEntity.ok(matchService.refreshMatchesByPuuid(puuid));
     }
 }
