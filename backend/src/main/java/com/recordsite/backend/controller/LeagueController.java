0f9f6495bfbf71262a525808e6dbd64a6a735a65
KimGyuhyun <kgh980605@gmail.com>
2026-06-13 17:23:03 +0900

    fix: Unused due to feature change

--- backend/src/main/java/com/recordsite/backend/controller/LeagueController.java
+++ backend/src/main/java/com/recordsite/backend/controller/LeagueController.java
@@ -18,10 +18,10 @@ public class LeagueController {
 
     private final LeagueService leagueService;
 
-    // puuid로 솔로랭크/자유랭크 정보 조회
-    @GetMapping("/entries")
-    public ResponseEntity<List<RiotLeagueEntryResponse>> getLeagueEntries(
-            @RequestParam String puuid) {
-        return ResponseEntity.ok(leagueService.getLeagueEntriesByPuuid(puuid));
-    }
+//    // puuid로 솔로랭크/자유랭크 정보 조회
+//    @GetMapping("/entries")
+//    public ResponseEntity<List<RiotLeagueEntryResponse>> getLeagueEntries(
+//            @RequestParam String puuid) {
+//        return ResponseEntity.ok(leagueService.getLeagueEntriesByPuuid(puuid));
+//    }
 }
