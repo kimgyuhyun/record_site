1f2faa4898d301d4237a25d3dc556b1bc963cd1f
KimGyuhyun <kgh980605@gmail.com>
2026-06-16 15:29:16 +0900

    feat: Added a method that returns a matchIdSet that duplicates the matchIdList passed using JPQL

--- backend/src/main/java/com/recordsite/backend/repository/MatchRepository.java
+++ backend/src/main/java/com/recordsite/backend/repository/MatchRepository.java
@@ -4,10 +4,21 @@ import com.recordsite.backend.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
     Match findByMatchId(String matchId);
+
+    @Query("""
+            SELECT m.matchId
+            FROM Match m
+            WHERE m.matchId IN :matchIds
+            """)
+    Set<String> findExistingMatchIds(@Param("matchIds") List<String> matchIds);
 
 }
